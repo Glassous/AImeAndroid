cb1a28d326c3716488fbf9fe8b109328d0dee645
Glassous <143624737+Glassous@users.noreply.github.com>
2025-11-11 00:58:53 +0800

    修改了版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.glassous.aime"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
